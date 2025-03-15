217acf7c25575431f260db0a5330a703e0a7fbc4
Project Fumo <154620435+ProjectFumo@users.noreply.github.com>
2025-03-16 04:44:45 +1030

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,17 @@
+# This is not useful unless you are using NixOS https://nixos.org/
+
+# GLFW fix for IntelliJ on NixOS 24.11 with nix-shell.
+# To execute type: nix-shell
+
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.libGL
-  ];
+  pkgs.mkShell {
+    buildInputs = [
+      pkgs.libGL
+    ];
+
   shellHook = ''
-  export LD_LIBRARY_PATH="''${LD_LIBRARY_PATH}''${LD_LIBRARY_PATH:+:}${pkgs.libglvnd}/lib"
-  idea-community
+    export LD_LIBRARY_PATH="''${LD_LIBRARY_PATH}''${LD_LIBRARY_PATH:+:}${pkgs.libglvnd}/lib"
+    idea-community ./ &
   '';
 }
